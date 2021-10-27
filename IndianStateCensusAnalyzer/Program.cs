cd6126bdbe2fd4f744422e3ba86d03efc69e7619
b-s-ad <ahmedshehbazb@gmail.com>
2021-10-26 18:30:13 -0700

    TC 1.1 GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount
    GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indian Status Census!");
+            Console.ReadKey();
+        }
+    }
+}
